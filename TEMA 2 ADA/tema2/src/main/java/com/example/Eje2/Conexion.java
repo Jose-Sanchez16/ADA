0bfab606b52f893149c35bb414606ff8f5b0cc4a
Jose-Sanchez16 <jose.sanchez.moreno.al@iespoligonosur.org>
2025-11-04 16:22:49 +0100

    Realizado hasta el f del 2.3

--- TEMA 2 ADA/tema2/src/main/java/com/example/Eje2/Conexion.java
+++ TEMA 2 ADA/tema2/src/main/java/com/example/Eje2/Conexion.java
@@ -4,24 +4,29 @@ import java.sql.*;
 
 public class Conexion {
 
-    public static void main(String[] args) throws Exception{
-        Conexion conexion = new Conexion();
-        conexion.a();
+    public static void main(String[] args) throws Exception {
+        // a();
+        // b();
+        // c();
+        // d();
+        // e();
+        f();
     }
 
-    public static Connection con()throws Exception {
+    public static Connection con() throws Exception {
         String user = "root";
         String passwd = "1234";
         String url = "jdbc:mysql://localhost:3306/Empresa";
         return DriverManager.getConnection(url, user, passwd);
     }
 
-    public void a()throws Exception{
-        Connection conexion = con ();
+    public static void a() throws Exception {
+        Connection conexion = con();
         Statement st = conexion.createStatement();
-        ResultSet rs = st.executeQuery("Select comision, nombre , salario From EMPLEADOS Where num_hijos > 3 Order By comision, nombre;");
+        ResultSet rs = st.executeQuery(
+                "Select comision, nombre , salario From EMPLEADOS Where num_hijos > 3 Order By comision, nombre;");
 
-        while (rs.next()){
+        while (rs.next()) {
             int con = rs.getInt(1);
             String nom = rs.getString(2);
             int sal = rs.getInt(3);
@@ -29,4 +34,71 @@ public class Conexion {
             System.out.println("Comision: " + con + " Nombre: " + nom + " Salario: " + sal);
         }
     }
+
+    public static void b() throws Exception {
+        Connection conexion = con();
+        Statement st = conexion.createStatement();
+        ResultSet rs = st.executeQuery("Select nombre From DEPARTAMENTOS Where depto_jefe is null;");
+
+        while (rs.next()) {
+            String nom = rs.getString(1);
+
+            System.out.println(nom);
+        }
+    }
+
+    public static void c() throws Exception {
+        Connection conexion = con();
+        Statement st = conexion.createStatement();
+        ResultSet rs = st.executeQuery(
+                "Select nombre, salario From EMPLEADOS Where salario Between 1250 and 1300 Order By nombre;");
+
+        while (rs.next()) {
+            String nom = rs.getString(1);
+            int sal = rs.getInt(2);
+
+            System.out.println(nom + ", " + sal);
+        }
+    }
+
+    public static void d() throws Exception {
+        Connection conexion = con();
+        Statement st = conexion.createStatement();
+        ResultSet rs = st.executeQuery(
+                "Select nombre, salario From EMPLEADOS Where salario Between 1250 and 1300 Or num_hijos >= 1 Order By nombre;");
+
+        while (rs.next()) {
+            String nom = rs.getString(1);
+            int sal = rs.getInt(2);
+
+            System.out.println(nom + ", " + sal);
+        }
+    }
+
+    public static void e() throws Exception {
+        Connection conexion = con();
+        Statement st = conexion.createStatement();
+        ResultSet rs = st.executeQuery(
+                "Select nombre From DEPARTAMENTOS Where nombre not like 'DIRECCION%' and nombre not like 'SECTOR%' order by nombre ;");
+
+        while (rs.next()) {
+            String nom = rs.getString(1);
+
+            System.out.println(nom);
+        }
+    }
+
+    public static void f() throws Exception {
+        Connection conexion = con();
+        Statement st = conexion.createStatement();
+        ResultSet rs = st.executeQuery(
+                "Select nombre From DEPARTAMENTOS Where (tipo_dir = 'F' and presupuesto <= 5) or depto_jefe is null order by nombre;");
+
+        while (rs.next()) {
+            String nom = rs.getString(1);
+
+            System.out.println(nom);
+        }
+    }
+
 }
