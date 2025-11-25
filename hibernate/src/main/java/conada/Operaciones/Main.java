f0ddfb5213c9cdf59f877b1e60c09cebe0e8da75
Jose-Sanchez16 <jose.sanchez.moreno.al@iespoligonosur.org>
2025-11-25 17:07:35 +0100

    Hibertanate añadir y borrar

--- hibernate/src/main/java/conada/Operaciones/Main.java
+++ hibernate/src/main/java/conada/Operaciones/Main.java
@@ -8,16 +8,32 @@ import jakarta.persistence.*;
 public class Main {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("miBD");
-        EntityManager em = emf.createEntityManager();
 
-        Persona persona1 = new Persona (101, "Ana Gracia", "Calle Falsa 123", LocalDate.of(2000, 7, 15));
+        Persona p1 = new Persona(4, "Ana Gracia", "Calle Falsa 123", LocalDate.of(2000, 7, 15));
+        Persona p2 = new Persona(5, "Agapito Gracia", "Calle Verdad 123", LocalDate.of(2001, 2, 1));
 
+        borrarPersona(emf.createEntityManager(), 4);
+        añadirPersona(emf.createEntityManager(), p2);
+        añadirPersona(emf.createEntityManager(), p1);
+    }
+
+    public static void añadirPersona(EntityManager em, Persona p) {
         EntityTransaction et = em.getTransaction();
         et.begin();
 
-        em.persist(persona1);
+        em.persist(p);
+
         et.commit();
 
+        em.close();
+    }
+    public static void borrarPersona(EntityManager em , int id){
+        EntityTransaction et = em.getTransaction();
+        et.begin();
+
+        Persona p = em.find (Persona.class, id);
+        em.remove(p);
+        et.commit();
         em.close();
     }
 }
